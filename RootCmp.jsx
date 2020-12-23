47cfb97bec301fb74c0f77395cc3046a06e10809
udikashri <74731979+udikashri@users.noreply.github.com>
2020-12-23 11:38:53 +0200

    Add files via upload

---
+++ RootCmp.jsx
@@ -0,0 +1,32 @@
+import { AppHeader } from './cmps/AppHeader.jsx';
+import { About } from './pages/About.jsx';
+import { Home } from './pages/Home.jsx';
+import { PetApp } from './pages/PetApp.jsx';
+import { PetDetails } from './pages/PetDetails.jsx';
+import { PetEdit } from './pages/PetEdit.jsx';
+import { Survey } from './pages/Survey.jsx';
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
+
+// Simple React Component
+export class RootCmp extends React.Component {
+
+    render() {
+        return (
+            <Router>
+                <section className="app">
+                    <AppHeader />
+                    <Switch>
+                        <Route path="/pet/edit/:petId?" component={PetEdit} />
+                        <Route path="/pet/:petId" component={PetDetails} />
+                        <Route path="/pet" component={PetApp} />
+                        <Route path="/about" component={About} />
+                        <Route path="/survey" component={Survey} />
+                        <Route path="/" component={Home} />
+                    </Switch>
+                    <footer className="animate__animated animate__jello">coffeerights 2020</footer>
+                </section>
+            </Router>
+        );
+    }
+}
---
+++ app.js
@@ -0,0 +1,7 @@
+import { RootCmp } from './RootCmp.jsx';
+import { networkPetService } from './services/networkPetService.js';
+
+ReactDOM.render(
+    <RootCmp />,
+    document.getElementById('root')
+);
